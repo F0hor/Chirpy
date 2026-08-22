28d84eb85356ce20600d57519906dc815c30c6fe
F0hor <f.hor@proton.me>
2026-08-22 22:00:22 +0200

    CH1 FIN

--- main.go
+++ main.go
@@ -3,10 +3,24 @@ package main
 import(
 	"net/http"
 	"time"
+	"fmt"
 )
 
 func main() {
+	fmt.Println("Stating server")
+
 	mux := http.NewServeMux()
+	mux.Handle(
+		"/app/", 
+		http.StripPrefix("/app", http.FileServer(http.Dir("./testFiles/"))),
+	)
+	mux.HandleFunc("/healthz", func( w http.ResponseWriter, r *http.Request){
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		
+		w.Write([]byte("OK"))
+})
+
 	serv := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
