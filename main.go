4facdd5d0ccf74561fa7bd29fdf5d6225b039cea
F0hor <f.hor@proton.me>
2026-08-26 17:52:06 +0200

    ch2.2

--- main.go
+++ main.go
@@ -4,22 +4,33 @@ import(
 	"net/http"
 	"time"
 	"fmt"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	fmt.Println("Stating server")
+	cfg := apiConfig{}
 
 	mux := http.NewServeMux()
 	mux.Handle(
 		"/app/", 
-		http.StripPrefix("/app", http.FileServer(http.Dir("./testFiles/"))),
+		cfg.middlewareMetricsInc(
+			http.StripPrefix("/app", http.FileServer(http.Dir("./testFiles/"))),
+		),
 	)
 	mux.HandleFunc("/healthz", func( w http.ResponseWriter, r *http.Request){
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		
 		w.Write([]byte("OK"))
-})
+	})
+
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerMetricsReset)
 
 	serv := &http.Server{
 		Addr: ":8080",
@@ -30,4 +41,27 @@ func main() {
 	}
 
 	serv.ListenAndServe()
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+		
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerMetricsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+		
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
